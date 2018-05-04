0c5909de18e8cdeeb9ac479c75796fe62868e7b1
장규승 <jgs0344@gmail.com>
2018-05-04 17:03:30 +0900

    Test commit

--- swift.sh
+++ swift.sh
@@ -11,7 +11,7 @@ OS_PROJECT_DOMAIN_ID="projectDomainId"
 OS_USER_DOMAIN_ID="userDomainId"
 publicURL="http://swift.test.com:8080/v1/AUTH_projectId"
 
-# 토큰 호출
+# get token
 getToken() {
     token=$(curl -i -s -X POST $OS_AUTH_URL/auth/tokens?nocatalog -H "Content-Type: application/json" -d \
     '{ "auth":
